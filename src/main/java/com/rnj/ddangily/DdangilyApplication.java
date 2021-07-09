f9bf3cd08871f7247f3f8ae5db6ebdb75263ae80
hwimer <hr.kim@infinigru.com>
2021-07-10 01:50:00 +0900

    add scheme

---
+++ src/main/java/com/rnj/ddangily/DdangilyApplication.java
@@ -0,0 +1,13 @@
+package com.rnj.ddangily;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DdangilyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DdangilyApplication.class, args);
+	}
+
+}
