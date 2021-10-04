c4b70febdf05fbbb4e9360ad57e9a0f37f931eed
hwimer <hr.kim@infinigru.com>
2021-10-04 22:32:06 +0900

    edit some config of mybatis generator

--- data/mybatis/gen.sh
+++ data/mybatis/gen.sh
@@ -5,9 +5,12 @@
 
 
 
+set -x
 
-rm -rf ../../src/main/resources/com/rnj/ddangily/persistence/InstagramMapper.xml
+#rm -rf ../../src/main/resources/com/rnj/ddangily/persistence/InstagramMapper.xml
 rm -rf ../../src/main/java/com/rnj/ddangily/persistence/InstagramMapper.java
-rm -rf ../../src/main/java/com/rnj/ddangily/model/Instagram.java
+rm -rf ../../src/main/java/com/rnj/ddangily/persistence/MasterConfigMapper.java
+#rm -rf ../../src/main/java/com/rnj/ddangily/model/Instagram.java
 
-java -jar mybatis-generator-core-1.3.7.jar -configfile generatorConfig.xml -overwrite
+# java -jar mybatis-generator-core-1.3.7.jar -configfile generatorConfig.xml -overwrite
+java -jar mybatis-generator-core-1.4.0.jar -configfile generatorConfig.xml -overwrite
