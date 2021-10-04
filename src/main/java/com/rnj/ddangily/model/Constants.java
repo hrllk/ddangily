3809719d52ec1264a924f82db90c9bf0ada6a851
hwimer <hr.kim@infinigru.com>
2021-10-04 23:14:00 +0900

    working

---
+++ src/main/java/com/rnj/ddangily/model/Constants.java
@@ -0,0 +1,8 @@
+package com.rnj.ddangily.model;
+
+public final class Constants {
+
+
+//    public static final String TOKENREFRESHURL = "http://graph.instagram.com/refresh_access_token?grant_type=ig_refresh_token&access_token={long-lived-access-token}";
+
+}
--- src/main/java/com/rnj/ddangily/schedule/Scheduler.java
+++ src/main/java/com/rnj/ddangily/schedule/Scheduler.java
@@ -1,21 +1,28 @@
 package com.rnj.ddangily.schedule;
 
+import com.rnj.ddangily.model.MasterConfig;
+import com.rnj.ddangily.model.MasterConfigExample;
 import com.rnj.ddangily.persistence.InstagramMapper;
+import com.rnj.ddangily.persistence.MasterConfigMapper;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import java.util.List;
+
 @Component
 public class Scheduler {
 
     Logger logger = LoggerFactory.getLogger(this.getClass().getName());
 
-    @Autowired
-    InstagramMapper instagramMapper;
-
 //    @Autowired
+//    InstagramMapper instagramMapper;
+
+    @Autowired
+    MasterConfigMapper masterConfigMapper;
+
     /***
      * 주기적으로 API 호출, 게시물을 가져오는 역할
      */
@@ -28,6 +35,10 @@ public class Scheduler {
     @Scheduled(fixedDelay = 5000)
     public void refreshToken(){
         logger.debug("START REFRESH TOKEN");
+        MasterConfigExample example = new MasterConfigExample();
+        example.createCriteria().andMasterKeyEqualTo("tokenRefreshUrl");
+        List<MasterConfig> masterConfigList = masterConfigMapper.selectByExample(example);
+        logger.debug("masterConfigList : [{}]", masterConfigList);
 
 
     }
